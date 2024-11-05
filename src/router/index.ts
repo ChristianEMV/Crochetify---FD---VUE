fa650482ae49700e7dd37ab348fe374370ced74b
moronissj <104938057+moronissj@users.noreply.github.com>
2024-11-04 23:30:17 -0600

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,18 +7,20 @@ import Home from "../views/Home.vue";
 //import Stock from "../views/Stock.vue";
 //import Sales from "../views/Sales.vue";
 //import Orders from "../views/Orders.vue";
-//import Shipments from "../views/Shipments.vue";
+import Shipments from "../views/Shipments.vue";
 //import Error from "../views/Error.vue";
+import Users from "../views/Users.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", name: "login", component: Login },
   { path: "/home", name: "home", component: Home },
+  { path: "/users", name:"users", component: Users},
   //{ path: "/categories", name: "categories", component: Categories },
   //{ path: "/products", name: "products", component: Products },
   //{ path: "/stock", name: "stock", component: Stock },
   //{ path: "/sales", name: "sales", component: Sales },
   //{ path: "/orders", name: "orders", component: Orders },
-  //{ path: "/shipments", name: "shipments", component: Shipments },
+  { path: "/shipments", name: "shipments", component: Shipments },
   //{ path: '/:pathMatch(.*)*', redirect: '/Error' }, // Para redirigir a login si la ruta no existe
 ];
 
