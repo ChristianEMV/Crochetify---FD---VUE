5ff53281aea1ef2a7e1086426112b87604271ada
ChristianEMV <20193tn065@utez.edu.mx>
2024-11-20 23:18:16 -0600

    [UPDATE] components.d.ts

--- my-site/components.d.ts
+++ my-site/components.d.ts
@@ -15,6 +15,7 @@ declare module 'vue' {
     BCol: typeof import('bootstrap-vue-next/components/BContainer')['BCol']
     BCollapse: typeof import('bootstrap-vue-next/components/BCollapse')['BCollapse']
     BForm: typeof import('bootstrap-vue-next/components/BForm')['BForm']
+    BFormCheckboxGroup: typeof import('bootstrap-vue-next/components/BFormCheckbox')['BFormCheckboxGroup']
     BFormGroup: typeof import('bootstrap-vue-next/components/BFormGroup')['BFormGroup']
     BFormInput: typeof import('bootstrap-vue-next/components/BFormInput')['BFormInput']
     BFormSelect: typeof import('bootstrap-vue-next/components/BFormSelect')['BFormSelect']
