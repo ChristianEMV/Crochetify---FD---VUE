f04b63f3b7dba3d46d16d9c197a7d136730cf2ab
ChristianEMV <113563752+ChristianEMV@users.noreply.github.com>
2024-11-29 00:12:03 -0600

    Update http-common.ts

--- my-site/src/http-common.ts
+++ my-site/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://35.153.187.92:8007/api/crochetify',
+  baseURL: 'http://35.153.187.92:8087/api/crochetify',
   headers: {
     'Content-Type': 'application/json',
   },
