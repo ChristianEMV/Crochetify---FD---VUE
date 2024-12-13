395fa6e7ff0c7e3400dec9ebec5248f13da2c6bb
ChristianEMV <20193tn065@utez.edu.mx>
2024-12-13 17:57:01 -0600

    Cambios SW

--- my-site/public/service-worker.js
+++ my-site/public/service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-cache-v3';
 const urlsToCache = [
     '/',
-    '/index.html',
+    './my-site/index.html',
     '/app.js',
     '/manifest.json',
     '/service-worker.js',
-    '/pouchdb.js',
+    './my-site/pouchdb.js',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/pouchdb/7.0.0/pouchdb.min.js',
     '/images/fallback.png' ,// Imagen de fallback si es necesario
