af476c03f424a3a8cb73ae119fa592776297c6ec
ChristianEMV <20193tn065@utez.edu.mx>
2024-12-13 18:16:22 -0600

    cambio 3

--- my-site/public/service-worker.js
+++ my-site/public/service-worker.js
@@ -1,15 +1,6 @@
 const CACHE_NAME = 'pwa-cache-v3';
 const urlsToCache = [
-    '/',
-    '../index.html',
-    '/app.js',
-    '/manifest.json',
-    '/service-worker.js',
-    '../pouchdb.js',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/pouchdb/7.0.0/pouchdb.min.js',
-    '/images/fallback.png' ,// Imagen de fallback si es necesario
-    'http://54.146.53.211:8007/categories'
+
 ];
 
 
